497f01e9a05e9368660c3a2ba5717f701acada6e
constance16 <constancejohn63@yahoo.com>
2024-11-08 10:05:45 +0000

    Added more files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,6 +15,9 @@ function App() {
     <>
       <div>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
