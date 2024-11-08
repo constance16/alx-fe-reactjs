c15037b142d7f4b0c513dd901f537a677d08dd6c
constance16 <constancejohn63@yahoo.com>
2024-11-08 10:16:29 +0000

    Added another file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -18,6 +19,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Mark" age="27" bio="Hiking" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+const UserProfile = (props) => {
+    return (
+      <div>
+        <h2>{Mark}</h2>
+        <p>Age: {27}</p>
+        <p>Bio: {Hiking}</p>
+      </div>
+    );
+  };
+
+  export default UserProfile;
