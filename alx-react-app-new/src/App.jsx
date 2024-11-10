19a6c4194c5b42d71b136ba8d68e1e83c87b20be
constance16 <constancejohn63@yahoo.com>
2024-11-10 00:31:41 +0000

    Updated file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Counter Application</h1>
+      <Counter />
+    </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
