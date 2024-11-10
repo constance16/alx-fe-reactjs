c551be8aa7af87984017a90bbddf6f186b978669
constance16 <constancejohn63@yahoo.com>
2024-11-10 18:24:46 +0000

    Correction

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -7,4 +7,17 @@ const UserProfile = (props) => {
       </div>
     );
   };
-  export default UserProfile;
+
+  /* eslint-disable no-unused-vars */
+import React, { useContext } from 'react';
+import UserContext from '../components/UserContext';
+
+
+<div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+   <h2 style={{ color: 'blue' }}>{name}</h2>
+   <p>Age: 26 <span style={{ fontWeight: 'bold' }}>{age}</span></p>
+   <p>Bio: Loves travel and photography {bio}</p>
+   <UserContext/>
+ </div>
+
+export default UserProfile;
