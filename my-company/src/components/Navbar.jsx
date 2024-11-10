c785f96eaf2490649875df50055105d9c9fc783b
constance16 <constancejohn63@yahoo.com>
2024-11-10 19:29:51 +0000

    Added inline CSS to navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#eee' }}>
+    <nav style={{ padding: '10px', backgroundColor: '#eeee', display:'flex', justifyContent: 'centre', }}>
       <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
