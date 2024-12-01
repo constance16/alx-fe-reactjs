839c2c5b5add21504c05d156d43adf025611a57e
constance16 <constancejohn63@yahoo.com>
2024-12-01 00:22:55 +0000

    Added a file

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  darkMode: false,
   theme: { extend: {} },
+  variants: { extend: {} },
   plugins: [],
 };
-
