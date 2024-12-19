35d32d044582a96b69ed77f5c5c7837a263e7683
constance16 <constancejohn63@yahoo.com>
2024-12-19 00:37:59 +0000

    Modified githubservice.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
+export const fetchUserData = async (username, location, minRepos, page = 1) => {
   const baseURL = `https://api.github.com/users/${username}`;
   try {
     const response = await axios.get(baseURL);
@@ -12,3 +12,28 @@ export const fetchUserData = async (username) => {
     throw new Error("Failed to fetch user data");
   }
 };
+  
+  if (username) {
+    queryParams.push(`in:login ${username}`);
+  }
+
+  if (location) {
+    queryParams.push(`location:${location}`);
+  }
+
+  if (minRepos) {
+    queryParams.push(`repos:>=${minRepos}`);
+  }
+
+  const queryString = queryParams.join("+");
+
+  try {
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${queryString}&per_page=10&page=${page}`
+    );
+    return response.data.items; // Return an array of matched users
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+;
